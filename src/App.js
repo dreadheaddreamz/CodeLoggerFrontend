b7fa37fe96b1f4a0cc5d86781192b441f5d1f3f9
dreadheaddreamz <github email address>
2021-07-19 07:50:38 -0500

    added some early routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import './App.css'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './components/Home'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1> This is working </h1>
-      </div>
+      <Router>
+        <Route exact path="/" component={Home}/>
+
+      </Router>
     )
   }
 }
