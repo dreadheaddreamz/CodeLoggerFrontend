0bb91e0f9e01a34a116e9992487b531becdd8da7
dreadheaddreamz <github email address>
2021-07-28 19:01:17 -0500

    fixed some css alignments

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,14 @@ import homeimage from '../images/homeimage.jpeg'
 
 const Home = () => {
         return (
-            <div>
+            <div className="homesection" >
                 <h1><strong>Code Task</strong></h1>
                 <img src={homeimage} alt="home" height={300} width={300} className="home-image" />
-                <p>
-                    Welcome to Code Log where you can track and keep notes of the progess you have made in your coding journey. 
+               <span>
+                    Welcome to Code Task where you can track and keep notes of the progess you have made in your coding journey. 
                     As well as keeping track of your progress, we also have a To Do list. You can use this list to prioritize what 
                     is most important to learn or anything else you see fit.
-                </p>
+                </span>
             </div>
         )
     }
